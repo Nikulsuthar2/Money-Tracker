9bc35dfb74c564bd033ad578d9baa52e2027a579
= <=>
2025-12-25 12:04:48 +0530

    App icons changed

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.moneymanager.money_manager"
+    namespace = "com.nikapps.money_manager"
     compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.moneymanager.money_manager"
+        applicationId = "com.nikapps.money_manager"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
